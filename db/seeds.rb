b78b33f1c61cfbafeea018330e28a5fec0a7bfa3
anshuman7negi <anshumannegi777@gmail.com>
2023-11-28 15:43:25 +0530

    Added api to get greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Greeting.create(message: 'Hello!')
+Greeting.create(message: 'Hi there!')
+Greeting.create(message: 'Greetings!')
+Greeting.create(message: 'Good morning')
+Greeting.create(message: 'Happy Coding!')
+Greeting.create(message: 'Congratulation!')
